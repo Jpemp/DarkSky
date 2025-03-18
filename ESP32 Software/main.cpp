70364a98e2824f60f246a42918bf72a1eeba0dd3
Jpemp <159839779+Jpemp@users.noreply.github.com>
2025-03-18 15:34:26 -0500

    Update main.cpp
    time_read function works now. can connect to NTP server and get time. Need to figure out changing scheduling and command stuff

--- ESP32 Software/main.cpp
+++ ESP32 Software/main.cpp
@@ -1,23 +1,28 @@
 #include <Arduino.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
-#include <SolarCalculator.h>
 #include <Wifi.h>
 #include <time.h>
 #include <stdlib.h>
 
-#include <string>
-
 using namespace std;
 
-const char *wifi_ID = "GuysHouse";
+const char *wifi_ID = "GuysHouse"; //set up with NETGEAR router
 const char *password = "Proverbs910";
 const char *ntpServer = "pool.ntp.org";
 
-double gmt = 0; //keep in universal time
-double daylight_savings = 3600;
+int schedule_time;
 
-struct tm universal_time;
+double utc = -6*3600; //time displayed as universal time. adjusted from greenwich mean time
+double daylight_savings = 3600; //account for daylight savings. Figure out how to change this when daylight savings is over 
+
+struct tm universal_time; //time struct to keep track of utc time on ESP32
+
+static int tempSensor_input = 26;
+static int fan_power = 27;
+static int SQM_power = 15;
+static int miniPC_power = 32;
+static int dewHeater_power = 14;
 
 
 // put function declarations here:
@@ -25,8 +30,9 @@ void temp_read(void); //record temperature. If it is too hot, fan will turn on
 void time_read(void);
 void communication();
 void WiFi_initializing(void);
+void socket_setup(void);
 
-OneWire oneWire(26); //GPIO 26/A0 is input for digital temp reader. Argument tells OneWire(DS18B20) which pin the reader info is going to 
+OneWire oneWire(tempSensor_input); //GPIO 26/A0 is input for digital temp reader. Argument tells OneWire(DS18B20) which pin the reader info is going to 
 DallasTemperature tempSensor(&oneWire); //passes GPIO address to DallasTemperature. Pointer parameter requires this. Address points to GPIO pin 34
 
 void setup() {
@@ -35,15 +41,14 @@ void setup() {
 
   //pin configuration
   pinMode(LED_BUILTIN, OUTPUT); //LED on the board. We can use this to indicate that the board is on
-  pinMode(27, OUTPUT); //Connect to fan relay (GPIO 27/A10 input on ADC2)
-  pinMode(15, OUTPUT); //Connect to SQM relay (GPIO 15/A8 input on ADC2)
-  pinMode(32, OUTPUT); //Connect to MiniPC relay (GPIO 32/A7 on ADC1/32KHz crystal)
-  pinMode(14, OUTPUT); //Connect to dew heater (GPIO 14/A6 on ADC2)
+  pinMode(fan_power, OUTPUT); //Connect to fan relay (GPIO 27/A10 input on ADC2)
+  pinMode(SQM_power, OUTPUT); //Connect to SQM relay (GPIO 15/A8 input on ADC2)
+  pinMode(miniPC_power, OUTPUT); //Connect to MiniPC relay (GPIO 32/A7 on ADC1/32KHz crystal)
+  pinMode(dewHeater_power, OUTPUT); //Connect to dew heater (GPIO 14/A6 on ADC2)
 
-  WiFi_initializing();
-  configTime(gmt, daylight_savings, ntpServer); //Configuring time
-
-  //Serial.println(universal_time->tm_hour);
+  WiFi_initializing(); //Connects ESP32 to WiFi
+  //socket_setup();
+  configTime(utc, daylight_savings, ntpServer); //Configuring time module to npt_server
 
   tempSensor.begin(); //intializes the DS18B20 sensor 
 
@@ -53,7 +58,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);
   //temp_read();
-  //time_read();
+  time_read();
   //digitalWrite(27, LOW);
   //digitalWrite(15, LOW);
   //digitalWrite(32, LOW);
@@ -88,17 +93,14 @@ void temp_read(void) {
 
 void time_read(void){
   getLocalTime(&universal_time);
-  time_read();
-  //std::time_t  universal_time = now();
-  //if(universal_time == midnight) then call calcSunriseSunset funciton
-  /*calcSunriseSunset(universal_time, lattitude, longitude, transit, sunrise, sunset);
+  Serial.print(universal_time.tm_hour);
+  Serial.print(":");
+  Serial.print(universal_time.tm_min);
+  Serial.print(":");
+  Serial.println(universal_time.tm_sec);
+  if(universal_time.tm_hour == (22||0||2)){
 
-  Serial.print("Sunrise: ");
-  Serial.println(sunrise);
-
-  Serial.print("Sunset: ");
-  Serial.println(sunset);*/
-
+  }
 }
 
 void WiFi_initializing(void){
@@ -113,7 +115,7 @@ void WiFi_initializing(void){
     digitalWrite(LED_BUILTIN, LOW);
     delay(250);
     if(fail_count >= 20){
-      Serial.print("Connection is taking too long! Exiting program.");
+      Serial.print("WiFi connection is taking too long! Exiting program.");
       exit(1); //if it's taking too long to connect to WiFi, end program
     }
     else{
@@ -125,4 +127,8 @@ void WiFi_initializing(void){
   digitalWrite(LED_BUILTIN, HIGH);
   delay(3000);
   digitalWrite(LED_BUILTIN, LOW);
+}
+
+void socket_setup(void){
+
 }
