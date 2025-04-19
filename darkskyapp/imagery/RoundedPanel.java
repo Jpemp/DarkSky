d82053a57ab928e82e8ff498c7e484e14b30b1d6
Jpemp <159839779+Jpemp@users.noreply.github.com>
2025-04-18 22:19:30 -0500

    Update RoundedPanel.java

--- darkskyapp/imagery/RoundedPanel.java
+++ darkskyapp/imagery/RoundedPanel.java
@@ -1,1 +1,25 @@
+package com.darksky.imagery;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundedPanel extends JPanel {
+    private final int cornerRadius;
+
+    public RoundedPanel(int cornerRadius) {
+        this.cornerRadius = cornerRadius;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(getBackground());
+        g2d.fillRoundRect(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius);
+    }
+}
